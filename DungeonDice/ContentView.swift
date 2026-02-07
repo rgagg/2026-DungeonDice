559e24adf06f6c547da628b1e312983c04d1064b
Richard Gagg <r.gagg@outlook.com>
2026-02-08 03:23:35 +1100

    Ch: 4.3. Inuial UI useing forEach to generate buttons.

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,17 +8,122 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  
+  @State private var message: String = "Roll a die!"
+  private let diceTypes: [Int] = [4, 6, 8, 10, 12, 20]
+  
+  var body: some View {
+    VStack {
+      Text("Dingeon Dice!")
+        .font(.largeTitle)
+        .fontWeight(.black)
+        .foregroundStyle(.red)
+      
+      Spacer()
+      
+      Text(message)
+        .font(.largeTitle)
+        .multilineTextAlignment(.center)
+      
+      Spacer()
+      
+      Group {
+        ForEach(diceTypes, id: \.self) { diceType in
+          Button {// 4 sided die
+            rollDie(sides: diceType)
+          } label: {
+            Text("\(diceType)-Sided")
+          }
         }
-        .padding()
+      }
+      .font(.title2)
+      .buttonStyle(.glassProminent)
+      .foregroundStyle(.white)
+      .tint(.red)
+
+
+
+      //          VStack {
+      //            HStack {
+      //              Button {// 4 sided die
+      //                rollDie(sides: 4)
+      //              } label: {
+      //                Text("4-Sided")
+      //              }
+      //
+      //              Spacer()
+      //
+      //              Button {// 6 sided die
+      //                rollDie(sides: 6)
+      //              } label: {
+      //                Text("6-Sided")
+      //              }
+      //
+      //              Spacer()
+      //
+      //              Button {// 8 sided die
+      //                rollDie(sides: 8)
+      //              } label: {
+      //                Text("8-Sided")
+      //              }
+      //
+      //            }
+      //            HStack {
+      //              Button {// 10 sided die
+      //                rollDie(sides: 10)
+      //              } label: {
+      //                Text("10-Sided")
+      //              }
+      //
+      //              Spacer()
+      //
+      //              Button {// 12 sided die
+      //                rollDie(sides: 12)
+      //              } label: {
+      //                Text("12-Sided")
+      //              }
+      //
+      //              Spacer()
+      //
+      //              Button {// 20 sided die
+      //                rollDie(sides: 20)
+      //              } label: {
+      //                Text("20-Sided")
+      //              }
+      //
+      //            }
+      //            HStack {
+      //              Button {// 100 sided die
+      //                rollDie(sides: 100)
+      //              } label: {
+      //                Text("100-Sided")
+      //              }
+      //
+      //            }
+      //          }
+      //          .font(.title2)
+      //          .buttonStyle(.glassProminent)
+      //          .foregroundStyle(.white)
+      //          .tint(.red)
     }
+    .padding()
+  }
+  
+  // MARK: Functions
+  
+  func rollDie(sides: Int) {
+    let roll = Int.random(in: 1...sides)
+    message = "You Rolled a \(roll) on a \(sides)-sided die."
+  }
+  
 }
 
-#Preview {
-    ContentView()
+#Preview("Light Mode") {
+  ContentView()
+    .preferredColorScheme(.light)
+}
+
+#Preview("Dark Mode") {
+  ContentView()
+    .preferredColorScheme(.dark)
 }
