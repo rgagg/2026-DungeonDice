f34f7abd793aeb81e0b676704079b28f0cab5f7b
Richard Gagg <r.gagg@outlook.com>
2026-06-07 14:23:44 +1000

    Initial UI

--- 2026-DungeonDice/ContentView.swift
+++ 2026-DungeonDice/ContentView.swift
@@ -8,15 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var message: String = "Roll the dice!"
+  
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      VStack {
+            Text("Dungeon Dice!")
+              .font(.largeTitle)
+              .fontWeight(.black)
+              .foregroundStyle(.red)
+            
+            Spacer()
+            
+            Text(message)
+              .font(.largeTitle)
+              .multilineTextAlignment(.center)
+
+            Spacer()
+            
+            HStack {
+              Button {
+                dieRoll(sides: 4)
+              } label: {
+                Text("4 Sided")
+              }
+              .buttonStyle(.glassProminent)
+              .font(.title2)
+              .tint(.red)
+              
+
+              Button {
+                dieRoll(sides: 6)
+              } label: {
+                Text("6 Sided")
+              }
+              .buttonStyle(.glassProminent)
+              .font(.title2)
+              .tint(.red)
+              
+              Button {
+                dieRoll(sides: 8)
+              } label: {
+                Text("8 Sided")
+              }
+              .buttonStyle(.glassProminent)
+              .font(.title2)
+              .tint(.red)
+            }
+            
+            HStack {
+              Button {
+                dieRoll(sides: 10)
+              } label: {
+                Text("10 Sided")
+              }
+              .buttonStyle(.glassProminent)
+              .font(.title2)
+              .tint(.red)
+              
+
+              Button {
+                dieRoll(sides: 12)
+              } label: {
+                Text("12 Sided")
+              }
+              .buttonStyle(.glassProminent)
+              .font(.title2)
+              .tint(.red)
+              
+              Button {
+                dieRoll(sides: 20)
+              } label: {
+                Text("20 Sided")
+              }
+              .buttonStyle(.glassProminent)
+              .font(.title2)
+              .tint(.red)
+            }
+
+            Button {
+              dieRoll(sides: 100)
+            } label: {
+              Text("100 Sided")
+            }
+            .buttonStyle(.glassProminent)
+            .font(.title2)
+            .tint(.red)
+            
+          }
+          .padding()
         }
-        .padding()
-    }
+        
+      func dieRoll(sides: Int) {
+        message = "You rolled a \(Int.random(in: 1...sides)) on a \(sides) sided die"
+        }
 }
 
 #Preview {
