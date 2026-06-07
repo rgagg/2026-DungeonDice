ecac28cad4a846712bb52d927a80dbfc1f92ace9
Richard Gagg <r.gagg@outlook.com>
2026-06-07 14:12:46 +1000

    Initial Commit

---
+++ 2026-DungeonDice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2026-DungeonDice
+//
+//  Created by Richard Gagg on 7/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2026-DungeonDice/_026_DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  _026_DungeonDiceApp.swift
+//  2026-DungeonDice
+//
+//  Created by Richard Gagg on 7/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct _026_DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
